58a6f80e3f0399949aa01f141172cc41ea94943a
sheum <37168380+sheumhossain@users.noreply.github.com>
2019-01-31 23:05:55 +0600

    Add files via upload

---
+++ Encap_test.java
@@ -0,0 +1,11 @@
+package encap;
+
+public class Encap_test {
+	public static void main(String args[]) {
+		Test name = new Test();
+		name.name1="sheum";
+		System.out.println(name.name1);
+		name.setName2("SHEUM HOSSAIN");
+		System.out.println(name.getName2());
+	}
+}
