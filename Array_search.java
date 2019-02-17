baea7131cfada7cefacebf4598d028a505e9cb3f
sheum <37168380+sheumhossain@users.noreply.github.com>
2019-02-17 16:32:41 +0600

    Add files via upload

---
+++ Array_search.java
@@ -0,0 +1,18 @@
+package ARRAY;
+
+public class Array_search {
+	public static void main(String args[]) {
+		int[] arr  = {1,2,3,4,5,6} ;
+		
+		/*int size = arr.length;
+		for(int i=0;i<size;i++) {
+			System.out.println(arr[i]);
+		}*/
+		int a=10;
+		a=4-2*a;
+		a=a+1;
+		System.out.println(a);
+		
+	}
+	
+}
